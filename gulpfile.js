e929bb499e253948251fb06c3ffdc58573b3a147
Stan Lewis <slewis@fusesource.com>
2015-04-24 08:31:22 -0400

    Minify site, add server-site task to test minified site locally

--- gulpfile.js
+++ gulpfile.js
@@ -344,21 +344,176 @@ gulp.task('reload', function() {
     .pipe(hawtio.reload());
 });
 
-gulp.task('site', ['clean', 'build'], function() {
-  gulp.src(['index.html', 'osconsole/config.js.tmpl', 'css/**', 'images/**', 'img/**', 'libs/**', 'dist/**'], {base: '.'}).pipe(gulp.dest('site'));
+gulp.task('site-fonts', function() {
+  return gulp.src(['libs/**/*.woff', 'libs/**/*.woff2', 'libs/**/*.ttf'], { base: '.' })
+    .pipe(plugins.flatten())
+    .pipe(plugins.debug({title: 'site font files'}))
+    .pipe(gulp.dest('site/fonts'));
+});
 
+gulp.task('tweak-open-sans', ['site-fonts'], function() {
+  return gulp.src('site/fonts/OpenSans*')
+    .pipe(plugins.flatten())
+    .pipe(gulp.dest('site/fonts/Open-Sans'));
+});
+
+gulp.task('tweak-droid-sans-mono', ['site-fonts'], function() {
+  return gulp.src('site/fonts/DroidSansMono*')
+    .pipe(plugins.flatten())
+    .pipe(gulp.dest('site/fonts/Droid-Sans-Mono'));
+});
+
+gulp.task('site-files', ['tweak-open-sans', 'tweak-droid-sans-mono'], function() {
+  return gulp.src(['images/**', 'img/**', 'libs/**/*.swf'], {base: '.'})
+    .pipe(plugins.debug({title: 'site files'}))
+    .pipe(gulp.dest('site'));
+
+});
+
+gulp.task('usemin', ['site-files'], function() {
+  return gulp.src('index.html')
+    .pipe(plugins.usemin({
+      css: [plugins.minifyCss(), 'concat'],
+      js: [plugins.uglify(), plugins.rev()]
+    }))
+    .pipe(plugins.debug({title: 'usemin'}))
+    .pipe(gulp.dest('site'));
+});
+
+gulp.task('site', ['usemin'], function() {
+  gulp.src('site/index.html')
+    .pipe(plugins.rename('404.html'))
+    .pipe(gulp.dest('site'));
+  gulp.src(['img/**', 'osconsole/config.js.tmpl'], { base: '.' })
+    .pipe(gulp.dest('site'));
   var dirs = fs.readdirSync('./libs');
+  var patterns = [];
   dirs.forEach(function(dir) {
     var path = './libs/' + dir + "/img";
     try {
       if (fs.statSync(path).isDirectory()) {
         console.log("found image dir: " + path);
         var pattern = 'libs/' + dir + "/img/**";
-        gulp.src([pattern]).pipe(gulp.dest('site/img'));
+        patterns.push(pattern);
       }
     } catch (e) {
       // ignore, file does not exist
     }
+  });
+  return gulp.src(patterns).pipe(plugins.debug({ title: 'img-copy' })).pipe(gulp.dest('site/img'));
+});
+
+gulp.task('serve-site', function() {
+  // lets disable unauthorised TLS issues with kube REST API
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  var kubeBase = process.env.KUBERNETES_MASTER || 'https://localhost:8443';
+  var kube = uri(urljoin(kubeBase, 'api'));
+  var osapi = uri(urljoin(kubeBase, 'osapi'));
+  console.log("Connecting to Kubernetes on: " + kube);
+  var staticAssets = [{
+      path: '/',
+      dir: 'site/'
+  }];
+  var dirs = fs.readdirSync('site/libs');
+  dirs.forEach(function(dir) {
+    dir = 'site/libs/' + dir;
+    console.log("dir: ", dir);
+    if (fs.statSync(dir).isDirectory()) {
+      console.log("Adding directory to search path: ", dir);
+      staticAssets.push({
+        path: '/',
+        dir: dir
+      });
+    }
+  });
+  var localProxies = [];
+  if (process.env.LOCAL_APP_LIBRARY === "true") {
+    localProxies.push({
+        proto: "http",
+        port: "8588",
+        hostname: "localhost",
+        path: '/kubernetes/api/v1beta2/proxy/services/app-library',
+        targetPath: "/"
+      });
+    console.log("because of $LOCAL_APP_LIBRARY being true we are using a local proxy for /kubernetes/api/v1beta2/proxy/services/app-library" );
+  }
+  if (process.env.LOCAL_FABRIC8_FORGE === "true") {
+    localProxies.push({
+        proto: "http",
+        port: "8080",
+        hostname: "localhost",
+        path: '/kubernetes/api/v1beta2/proxy/services/fabric8-forge',
+        targetPath: "/"
+      });
+    console.log("because of LOCAL_FABRIC8_FORGE being true we are using a local proxy for /kubernetes/api/v1beta2/proxy/services/fabric8-forge" );
+  }
+  if (process.env.LOCAL_GOGS_HOST) {
+    var gogsPort = process.env.LOCAL_GOGS_PORT || "3000";
+    //var gogsHostName = process.env.LOCAL_GOGS_HOST + ":" + gogsPort;
+    var gogsHostName = process.env.LOCAL_GOGS_HOST;
+    console.log("Using gogs host: " + gogsHostName);
+    localProxies.push({
+        proto: "http",
+        port: gogsPort,
+        hostname: gogsHostName,
+        path: '/kubernetes/api/v1beta2/proxy/services/gogs-http-service',
+        targetPath: "/"
+      });
+    console.log("because of LOCAL_GOGS_HOST being set we are using a local proxy for /kubernetes/api/v1beta2/proxy/services/gogs-http-service to point to http://"
+    + process.env.LOCAL_GOGS_HOST + ":" + gogsPort);
+  }
+  var defaultProxies = [{
+    proto: kube.protocol(),
+    port: kube.port(),
+    hostname: kube.hostname(),
+    path: '/kubernetes/api',
+    targetPath: kube.path()
+  }, {
+    proto: osapi.protocol(),
+    port: osapi.port(),
+    hostname: osapi.hostname(),
+    path: '/kubernetes/osapi',
+    targetPath: osapi.path()
+  }, {
+    proto: kube.protocol(),
+    hostname: kube.hostname(),
+    port: kube.port(),
+    path: '/jolokia',
+    targetPath: '/hawtio/jolokia'
+  }, {
+    proto: kube.protocol(),
+    hostname: kube.hostname(),
+    port: kube.port(),
+    path: '/git',
+    targetPath: '/hawtio/git'
+  }];
+
+  var staticProxies = localProxies.concat(defaultProxies);
+  hawtio.setConfig({
+    port: 2772,
+    staticProxies: staticProxies,
+    staticAssets: staticAssets,
+    fallback: 'site/404.html',
+    liveReload: {
+      enabled: false
+    }
+  });
+  var debugLoggingOfProxy = process.env.DEBUG_PROXY === "true";
+  hawtio.use('/osconsole/config.js', function(req, res, next) {
+    var configJs = 'window.OPENSHIFT_CONFIG = {' +
+      ' auth: {' +
+      '   oauth_authorize_uri: "' + urljoin(kubeBase, '/oauth/authorize')  + '",' +
+      '   oauth_client_id: "fabric8-console",' +
+      ' }' +
+      '};';
+    res.set('Content-Type', 'application/javascript');
+    res.send(configJs);
+  });
+  hawtio.listen(function(server) {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("started from gulp file at ", host, ":", port);
   });
 });
 
