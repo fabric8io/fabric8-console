a38e49402eca10c78e9e3a5a30a0575981909001
Stan Lewis <slewis@fusesource.com>
2015-05-29 11:01:29 -0400

    Try fixing docker build

--- gulpfile.js
+++ gulpfile.js
@@ -394,7 +394,7 @@ gulp.task('site', ['usemin'], function() {
   gulp.src('site/index.html')
     .pipe(plugins.rename('404.html'))
     .pipe(gulp.dest('site'));
-  gulp.src(['img/**', 'osconsole/config.js.tmpl'], { base: '.' })
+  gulp.src(['img/**', 'osconsole/config.js.tmpl', 'apiman/config.js'], { base: '.' })
     .pipe(gulp.dest('site'));
   var dirs = fs.readdirSync('./libs');
   var patterns = [];
