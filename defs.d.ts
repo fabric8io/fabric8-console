95927257f01349b602cb589786289a35f3e741ad
James Strachan <james.strachan@gmail.com>
2015-10-12 10:20:44 +0100

    updated build

--- defs.d.ts
+++ defs.d.ts
@@ -1,9 +1,9 @@
 /// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
 /// <reference path="d.ts/forge/ts/forgeHelpers.d.ts"/>
-/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
 /// <reference path="d.ts/forge/ts/forgePlugin.d.ts"/>
 /// <reference path="d.ts/forge/ts/command.d.ts"/>
 /// <reference path="d.ts/forge/ts/commands.d.ts"/>
 /// <reference path="d.ts/forge/ts/repo.d.ts"/>
 /// <reference path="d.ts/forge/ts/repos.d.ts"/>
+/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
+/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
