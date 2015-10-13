79689e52d6191df9037edd921bcac2b997ce97bd
Stan Lewis <slewis@fusesource.com>
2015-10-13 15:47:17 -0400

    Remove hawtio-jmx/hawtio-wiki dependency, bring in java console as separate page

--- d.ts/includes.d.ts
+++ d.ts/includes.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="../libs/hawtio-utilities/defs.d.ts" />
-/// <reference path="../libs/hawtio-jmx/defs.d.ts" />
 /// <reference path="../libs/hawtio-oauth/defs.d.ts" />
 /// <reference path="../libs/hawtio-kubernetes/defs.d.ts" />
