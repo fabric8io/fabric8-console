9b1302eeb5d5c75ce082fa386a5a8aa5aaa26f2c
Stan Lewis <slewis@fusesource.com>
2015-10-15 10:22:37 -0400

    Try and simplify CI job, only push 'latest' image

--- maybeDeploy.sh
+++ maybeDeploy.sh
@@ -14,25 +14,17 @@ then
   echo "Deploying new build for $CURRENT"
   git config --global user.email "circleci@mail.com" && \
   git config --global user.name "circleci" && \
-  rm -Rf site/* && \
-  bower update && \
-  gulp site && \
-  gulp deploy && \
-  pushd .publish && \
-  git tag ${CURRENT}-build && \
-  git push && git push --tags && \
-  popd && \
   echo $CURRENT > LATEST && \
   git add LATEST && \
   git commit -m "Updating latest tag" && \
   git push && git push --tags && \
-  echo "Pushing fabric8/fabric8-console:${CURRENT} to Docker Hub" && \
-  git checkout ${CURRENT} && \
-  rm -Rf site/* && \
-  bower update && \
-  gulp site && \
-  docker build -t fabric8/fabric8-console:${CURRENT} . && \
-  docker push fabric8/fabric8-console:${CURRENT}
+  echo "Pushing fabric8/fabric8-console:latest to Docker Hub" && \
+  docker build -t fabric8/fabric8-console .
+  docker login -e $BINTRAY_EMAIL -u $BINTRAY_USER -p $BINTRAY_PASS fabric8-docker-fabric8.bintray.io
+  docker tag fabric8/fabric8-console fabric8-docker-fabric8.bintray.io/fabric8/fabric8-console
+  docker push fabric8-docker-fabric8.bintray.io/fabric8/fabric8-console
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+  docker push fabric8/fabric8-console
 # else
 #  echo "Re-tagging new build for ${CURRENT}"
 #  docker tag fabric8/fabric8-console fabric8/fabric8-console:${CURRENT}
