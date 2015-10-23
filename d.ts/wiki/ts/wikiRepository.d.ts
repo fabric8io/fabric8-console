47b7563a3cabaeb92ad4de26250426a4957adb20
James Strachan <james.strachan@gmail.com>
2015-10-23 11:16:30 +0100

    added generated interfaces

---
+++ d.ts/wiki/ts/wikiRepository.d.ts
@@ -0,0 +1,35 @@
+/// <reference path="../../includes.d.ts" />
+/// <reference path="wikiHelpers.d.ts" />
+/// <reference path="wikiPlugin.d.ts" />
+declare module Wiki {
+    class GitWikiRepository {
+        directoryPrefix: string;
+        private $http;
+        private config;
+        private baseUrl;
+        constructor($scope: any);
+        getPage(branch: string, path: string, objectId: string, fn: any): void;
+        putPage(branch: string, path: string, contents: string, commitMessage: string, fn: any): void;
+        history(branch: string, objectId: string, path: string, limit: number, fn: any): void;
+        commitInfo(commitId: string, fn: any): void;
+        commitTree(commitId: string, fn: any): void;
+        diff(objectId: string, baseObjectId: string, path: string, fn: any): void;
+        private doGet(path, query, successFn, errorFn?, config?);
+        private doPost(path, query, body, successFn, errorFn?);
+        private doPostForm(path, query, body, successFn, errorFn?);
+        getRepositoryLabel(fn: any, error: any): void;
+        exists(branch: string, path: string, fn: any): Boolean;
+        completePath(branch: string, completionText: string, directoriesOnly: boolean, fn: any): void;
+        putPageBase64(branch: string, path: string, contents: string, commitMessage: string, fn: any): void;
+        createDirectory(branch: string, path: string, commitMessage: string, fn: any): void;
+        revertTo(branch: string, objectId: string, blobPath: string, commitMessage: string, fn: any): void;
+        rename(branch: string, oldPath: string, newPath: string, commitMessage: string, fn: any): void;
+        removePage(branch: string, path: string, commitMessage: string, fn: any): void;
+        getPath(path: string): string;
+        getLogPath(path: string): string;
+        getContent(objectId: string, blobPath: string, fn: any): void;
+        branches(fn: any): void;
+        jsonChildContents(path: string, nameWildcard: string, search: string, fn: any): void;
+        git(): void;
+    }
+}
---
+++ d.ts/wiki/ts/wikiTopLevel.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../includes.d.ts" />
+/// <reference path="wikiHelpers.d.ts" />
+/// <reference path="wikiPlugin.d.ts" />
+declare module Wiki {
+    var TopLevelController: ng.IModule;
+}
