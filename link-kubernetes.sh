dbaa5aee3e041ec6545ec31f47cd0652e0414873
Stan Lewis <slewis@fusesource.com>
2015-10-29 11:10:10 -0400

    Add hawtio-ui link too

--- link-kubernetes.sh
+++ link-kubernetes.sh
@@ -6,7 +6,14 @@ ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.css libs/hawtio-kubernet
 echo "Now the libs/hawtio-kubernetes/dist folder is:"
 ls -l libs/hawtio-kubernetes/dist
 
+# hawtio-forms
 ln -fs `pwd`/../hawtio-forms/dist/hawtio-forms.js libs/hawtio-forms/dist/hawtio-forms.js
 ln -fs `pwd`/../hawtio-forms/dist/hawtio-forms.css libs/hawtio-forms/dist/hawtio-forms.css
-echo "Hawtio forms linked too..."
+echo "hawtio-ui linked too..."
 ls -la libs/hawtio-forms/dist
+
+# hawtio-ui
+ln -fs `pwd`/../hawtio-ui/dist/hawtio-ui.js libs/hawtio-ui/dist/hawtio-ui.js
+ln -fs `pwd`/../hawtio-ui/dist/hawtio-ui.css libs/hawtio-ui/dist/hawtio-ui.css
+echo "hawtio-ui linked too..."
+ls -la libs/hawtio-ui/dist
