0435600590443f57e521633ff38e225432cbf7b0
James Strachan <james.strachan@gmail.com>
2015-11-02 10:11:56 +0000

    updated hawtio-kubernetes

--- defs.d.ts
+++ defs.d.ts
@@ -1,8 +1,10 @@
 /// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
 /// <reference path="d.ts/forge/ts/forgeHelpers.d.ts"/>
-/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
-/// <reference path="d.ts/main/ts/about.d.ts"/>
+/// <reference path="d.ts/forge/ts/forgePlugin.d.ts"/>
+/// <reference path="d.ts/forge/ts/command.d.ts"/>
+/// <reference path="d.ts/forge/ts/commands.d.ts"/>
+/// <reference path="d.ts/forge/ts/repo.d.ts"/>
+/// <reference path="d.ts/forge/ts/repos.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiHelpers.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiPlugin.d.ts"/>
 /// <reference path="d.ts/wiki/ts/camel.d.ts"/>
@@ -21,8 +23,6 @@
 /// <reference path="d.ts/wiki/ts/wikiNavigation.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiRepository.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiTopLevel.d.ts"/>
-/// <reference path="d.ts/forge/ts/forgePlugin.d.ts"/>
-/// <reference path="d.ts/forge/ts/command.d.ts"/>
-/// <reference path="d.ts/forge/ts/commands.d.ts"/>
-/// <reference path="d.ts/forge/ts/repo.d.ts"/>
-/// <reference path="d.ts/forge/ts/repos.d.ts"/>
+/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
+/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
+/// <reference path="d.ts/main/ts/about.d.ts"/>
