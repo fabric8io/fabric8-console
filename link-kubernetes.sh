ba031350b6eb0f36a2d75a916dbc5ef091108075
James Strachan <james.strachan@gmail.com>
2015-11-17 15:46:28 +0000

    lets keep link-kubernetes.sh to just linking to hawio-kubernetes and the link-all.sh script linking to all the hawtio dependencies

--- link-kubernetes.sh
+++ link-kubernetes.sh
@@ -6,30 +6,4 @@ ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.css libs/hawtio-kubernet
 echo "Now the libs/hawtio-kubernetes/dist folder is:"
 ls -l libs/hawtio-kubernetes/dist
 
-# hawtio-kubernetes-api
-ln -fs `pwd`/../hawtio-kubernetes-api/dist/hawtio-kubernetes-api.js libs/hawtio-kubernetes-api/dist/hawtio-kubernetes-api.js
-echo "hawtio-kubernetes-api linked too..."
-ls -la libs/hawtio-kubernetes-api/dist
 
-# hawtio-forms
-ln -fs `pwd`/../hawtio-forms/dist/hawtio-forms.js libs/hawtio-forms/dist/hawtio-forms.js
-ln -fs `pwd`/../hawtio-forms/dist/hawtio-forms.css libs/hawtio-forms/dist/hawtio-forms.css
-echo "hawtio-forms linked too..."
-ls -la libs/hawtio-forms/dist
-
-# hawtio-ui
-ln -fs `pwd`/../hawtio-ui/dist/hawtio-ui.js libs/hawtio-ui/dist/hawtio-ui.js
-ln -fs `pwd`/../hawtio-ui/dist/hawtio-ui.css libs/hawtio-ui/dist/hawtio-ui.css
-ln -fs `pwd`/../hawtio-ui/dist/site-base.css libs/hawtio-ui/dist/site-base.css
-echo "hawtio-ui linked too..."
-ls -la libs/hawtio-ui/dist
-
-# hawtio-core
-ln -fs `pwd`/../hawtio-core/hawtio-core.js libs/hawtio-core/hawtio-core.js
-echo "hawtio-core linked too..."
-ls -la libs/hawtio-core
-
-# hawtio-oauth
-ln -fs `pwd`/../hawtio-oauth/dist/hawtio-oauth.js libs/hawtio-oauth/dist/hawtio-oauth.js
-echo "hawtio-oauth linked too..."
-ls -la libs/hawtio-oauth/dist
