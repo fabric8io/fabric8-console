add685c971301cc5de1cf58ae96d4165bdde6ba3
James Strachan <james.strachan@gmail.com>
2015-11-21 06:25:16 +0000

    fixed link script so that it links to the d.ts files too so that we don't get duff compile errors when linking

--- link-kubernetes.sh
+++ link-kubernetes.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo "Linking the hawtio-kubernetes.js to the local build"
 rm libs/hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
+rm -rf libs/hawtio-kubernetes/d.ts
 ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.js
 ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.css libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
+ln -s `pwd`/../hawtio-kubernetes/d.ts libs/hawtio-kubernetes
 echo "Now the libs/hawtio-kubernetes/dist folder is:"
 ls -l libs/hawtio-kubernetes/dist
 
