7124b5cf5a143225c9ee47bc16209a02eb191a00
Stan Lewis <slewis@fusesource.com>
2015-12-08 11:29:16 -0500

    Fix link script

--- link-kubernetes.sh
+++ link-kubernetes.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Linking the hawtio-kubernetes.js to the local build"
-rm libs/hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
-rm -rf libs/hawtio-kubernetes/d.ts
-ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.js
-ln -s `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.css libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
-ln -s `pwd`/../hawtio-kubernetes/d.ts libs/hawtio-kubernetes
+#rm libs/hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
+#rm -rf libs/hawtio-kubernetes/d.ts
+ln -fs `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.js libs/hawtio-kubernetes/dist/hawtio-kubernetes.js
+ln -fs `pwd`/../hawtio-kubernetes/dist/hawtio-kubernetes.css libs/hawtio-kubernetes/dist/hawtio-kubernetes.css
+#ln -s `pwd`/../hawtio-kubernetes/d.ts libs/hawtio-kubernetes
 echo "Now the libs/hawtio-kubernetes/dist folder is:"
-ls -l libs/hawtio-kubernetes/dist
+ls -la libs/hawtio-kubernetes/dist
 
 
