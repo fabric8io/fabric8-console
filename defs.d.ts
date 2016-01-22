2d5dfbb5d674281f3e3084d2885b08f375f24d40
James Strachan <james.strachan@gmail.com>
2016-01-22 16:52:33 +0000

    update to latest hawtio

--- defs.d.ts
+++ defs.d.ts
@@ -17,9 +17,6 @@
 /// <reference path="d.ts/forge/ts/repo.d.ts"/>
 /// <reference path="d.ts/forge/ts/repos.d.ts"/>
 /// <reference path="d.ts/forge/ts/secrets.d.ts"/>
-/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
-/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
-/// <reference path="d.ts/main/ts/about.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiHelpers.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiPlugin.d.ts"/>
 /// <reference path="d.ts/wiki/ts/camel.d.ts"/>
@@ -41,3 +38,6 @@
 /// <reference path="d.ts/wiki/ts/wikiNavigation.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiRepository.d.ts"/>
 /// <reference path="d.ts/wiki/ts/wikiTopLevel.d.ts"/>
+/// <reference path="d.ts/main/ts/mainGlobals.d.ts"/>
+/// <reference path="d.ts/main/ts/mainPlugin.d.ts"/>
+/// <reference path="d.ts/main/ts/about.d.ts"/>
