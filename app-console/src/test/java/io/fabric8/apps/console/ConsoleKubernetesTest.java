4b814b443b9f8038a16b59429369beab180f5e2a
James Strachan <james.strachan@gmail.com>
2016-06-30 23:41:01 +0100

    update version

--- app-console/src/test/java/io/fabric8/apps/console/ConsoleKubernetesTest.java
+++ app-console/src/test/java/io/fabric8/apps/console/ConsoleKubernetesTest.java
@@ -39,8 +39,8 @@ public class ConsoleKubernetesTest {
     @Test
     public void testConsole() throws Exception {
         String serviceName = "fabric8";
-        assertThat(client).replicationController(serviceName, session.getNamespace()).isNotNull();
-        assertThat(client).hasServicePort(serviceName, session.getNamespace(), 80);
+        assertThat(client).replicationController(serviceName).isNotNull();
+        assertThat(client).hasServicePort(serviceName, 80);
 
         assertThat(client).pods()
                 .runningStatus()
