b750ba011af111c2a7934966b6fb5f526c7d3652
James Strachan <james.strachan@gmail.com>
2016-07-28 12:05:42 +0100

    lets update the console for kubernetes to configure API proxying so that the console can work via nodePorts or external IPs

--- app-kubernetes/src/main/java/io/fabric8/apps/console/ConsoleModelProcessor.java
+++ app-kubernetes/src/main/java/io/fabric8/apps/console/ConsoleModelProcessor.java
@@ -16,15 +16,46 @@
 
 package io.fabric8.apps.console;
 
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.EnvVarBuilder;
 import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
 import io.fabric8.kubernetes.generator.annotation.KubernetesModelProcessor;
 
 import java.util.Arrays;
+import java.util.List;
 
 @KubernetesModelProcessor
 public class ConsoleModelProcessor {
     int jenkinshiftPort = 9191;
+
+    public void onPodSpec(PodSpecBuilder builder) {
+        List<Container> containers = builder.getContainers();
+        if (containers.isEmpty()) {
+            System.out.println("WARNING: no containers to modify!");
+        } else {
+            Container container = containers.get(0);
+            container.setCommand(Arrays.asList("/kuisp",
+                    "-p",
+                    "9090",
+                    "-c",
+                    "/site/osconsole/config.basic.js.tmpl=/site/osconsole/config.js",
+                    "--bearer-token=/var/run/secrets/kubernetes.io/serviceaccount/token",
+                    "--skip-cert-validation=true",
+                    "--default-page=/index.html",
+                    "--max-age=24h",
+                    "-s",
+                    "/k8s/=https://kubernetes/",
+                    "--compress"
+            ));
+            List<EnvVar> envVars = container.getEnv();
+            envVars.add(new EnvVarBuilder().withName("KUBERNETES_MASTER_URI").withValue("/k8s").build());
+            container.setEnv(envVars);
+            builder.withContainers(containers);
+            System.out.println("Updated container command for " + container.getName() + " " + container.getImage() + " to: " + container.getCommand());
+        }
+    }
 
     public void onList(KubernetesListBuilder builder) {
         builder.addNewServiceAccountItem()
@@ -32,41 +63,41 @@ public class ConsoleModelProcessor {
                 .endServiceAccountItem()
                 .build();
 
-      builder.addNewServiceItem()
-              .withNewMetadata()
-              .withName("jenkinshift")
-              .addToLabels("group", "io.fabric8.apps")
-              .addToLabels("project", "console")
-              .addToLabels("provider", "fabric8")
-              .endMetadata()
-              .withNewSpec()
-              .withType("LoadBalancer")
-              .addNewPort()
-              .withName("http")
-              .withProtocol("TCP")
-              .withPort(80)
-              .withNewTargetPort(jenkinshiftPort)
-              .endPort()
-              .addToSelector("group", "io.fabric8.apps")
-              .addToSelector("project", "console")
-              .addToSelector("provider", "fabric8")
-              .endSpec()
-              .endServiceItem()
-              .build();
+        builder.addNewServiceItem()
+                .withNewMetadata()
+                .withName("jenkinshift")
+                .addToLabels("group", "io.fabric8.apps")
+                .addToLabels("project", "console")
+                .addToLabels("provider", "fabric8")
+                .endMetadata()
+                .withNewSpec()
+                .withType("LoadBalancer")
+                .addNewPort()
+                .withName("http")
+                .withProtocol("TCP")
+                .withPort(80)
+                .withNewTargetPort(jenkinshiftPort)
+                .endPort()
+                .addToSelector("group", "io.fabric8.apps")
+                .addToSelector("project", "console")
+                .addToSelector("provider", "fabric8")
+                .endSpec()
+                .endServiceItem()
+                .build();
     }
 
     public void onPodTemplateSpec(PodSpecBuilder builder) {
-      String jenkinshiftVersion = System.getProperty("project.version");
+        String jenkinshiftVersion = System.getProperty("project.version");
 
-      builder.addNewContainer()
-              .withName("jenkinshift")
-              .withImage("fabric8/jenkinshift:" + jenkinshiftVersion)
-              .addNewPort()
-              .withContainerPort(jenkinshiftPort)
-              .withProtocol("TCP")
-              .endPort()
-              .endContainer()
-              .build();
+        builder.addNewContainer()
+                .withName("jenkinshift")
+                .withImage("fabric8/jenkinshift:" + jenkinshiftVersion)
+                .addNewPort()
+                .withContainerPort(jenkinshiftPort)
+                .withProtocol("TCP")
+                .endPort()
+                .endContainer()
+                .build();
     }
 
 }
